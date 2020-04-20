b623cd6df5038423505c307985c4fe2022813edd
desenvolvedor <denisuba@gmailcom>
2020-04-20 17:03:35 -0300

    3

--- js/aplicacao.js
+++ js/aplicacao.js
@@ -9,13 +9,34 @@ let Conexoes = [
         'caminho': '192.168.254.212/3050:d:\\tek-system\\dados\\dadosmc_fb305.fdb'},
     {'id': 2,
         'apelido': 'Cliente 1097',
-        'caminho': '192.168.254.176/3053:/home/denis/dadosmc-1097.fdb'}
+        'caminho': '192.168.254.176/3053:/home/denis/dadosmc-1097.fdb'},
+    {'id': 3,
+        'apelido': 'Cliente 1045',
+        'caminho': '192.168.254.176/3053:/home/denis/dadosmc-1045.fdb'}
 ];
 
-let Pesquisas = [];
+let Pesquisas = [
+    {'id': 1,
+        'conexao': 1,
+        'apelido': 'Listar Pessoas',
+        'comando': 'select CODIGO_PESSOA, RAZAOSOCIAL_PESSOA from PESSOA'},
+    {'id': 2,
+        'conexao': 1,
+        'apelido': 'Listar Status',
+        'comando': 'select CODIGO_STATUS, DESCRICAO_STATUS from STATUS'},
+    {'id': 3,
+        'conexao': 2,
+        'apelido': 'Listar Clientes',
+        'comando': 'select CODIGO_PESSOA, RAZAOSOCIAL_PESSOA from PESSOA where CLIENTE_PESSOA = \'S\''},
+    {'id': 4,
+        'conexao': 2,
+        'apelido': 'Listar Bancos',
+        'comando': 'select CODIGO_BANCO, DESCRICAO_BANCO from BANCO'},
+];
 
 modalAbout_Botaoclose.addEventListener('click', (event) =>  {
     modalAbout.close();
+    FecharMenu();
 });
 
 // Tratamento de clicks no menu principal
@@ -66,8 +87,8 @@ function ListarConexoes () {
                         <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-item="${Conexao.id}" data-action="excluirConexao">
                           <i class="material-icons">delete</i>
                         </button>
-                        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-item="${Conexao.id}" data-action="AbrirConexao">
-                          <i class="material-icons">open</i>
+                        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-item="${Conexao.id}" data-action="abrirConexao">
+                          <i class="material-icons">open_in_new</i>
                         </button>
                       </div>
                     </div>
@@ -77,3 +98,36 @@ function ListarConexoes () {
 
     listaPrincipal.innerHTML = htmlConexoes;
 };
+
+function ListarPesquisas () {
+    var htmlPesquisas = '';
+    Pesquisas.forEach(Pesquisa => {
+        htmlPesquisas +=
+            `<li class="lista-principal-pesquisa">
+                <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+                    <div class="card-content-container" >  
+                      <div class="mdl-card__title">
+                        <h2 class="mdl-card__title-text">${Pesquisa.apelido}</h2>
+                      </div>
+                      <div class="mdl-card__supporting-text">
+                        ${Pesquisa.comando}
+                      </div>
+                                            
+                      <div class="mdl-card__menu">
+                        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-item="${Pesquisa.id}" data-action="editarPesquisa">
+                          <i class="material-icons">edit</i>
+                        </button>
+                        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-item="${Pesquisa.id}" data-action="excluirPesquisa">
+                          <i class="material-icons">delete</i>
+                        </button>
+                        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" data-item="${Pesquisa.id}" data-action="abrirPesquisa">
+                          <i class="material-icons">open_in_new</i>
+                        </button>
+                      </div>
+                    </div>
+                </div>
+            </li>`;
+    })
+
+    listaPrincipal.innerHTML = htmlPesquisas;
+}
