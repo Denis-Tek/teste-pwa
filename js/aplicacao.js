0014c89e08fe7998bbc996fd4ae4236345f4650b
desenvolvedor <denisuba@gmailcom>
2020-04-28 16:49:21 -0300

    Permitir editar

--- js/aplicacao.js
+++ js/aplicacao.js
@@ -1,14 +1,33 @@
 // Mapeamento DOM
-let listaPrincipal         = document.querySelector('.lista-principal');
 let botaoAdd               = document.querySelector('.botao-add');
+botaoAdd.style.display = 'none';
+
+let listaPrincipal         = document.querySelector('.lista-principal');
+
 let modalAbout             = document.querySelector('#dialogo-sobre');
 let modalConfirmarExclusao = document.querySelector('#dialogo-confirmar-exclusao');
+let modalEditarConexao     = document.querySelector('#dialogo-editar-conexao');
+let modalEditarPesquisa    = document.querySelector('#dialogo-editar-pesquisa');
+
+let editIdConexao          = modalEditarConexao.querySelector('#idConexao');
+let editApelidoConexao     = modalEditarConexao.querySelector('#apelidoConexao');
+let editCaminhoConexao     = modalEditarConexao.querySelector('#caminhoConexao');
+
+let editIdPesquisa          = modalEditarPesquisa.querySelector('#idPesquisa');
+let editApelidoPesquisa     = modalEditarPesquisa.querySelector('#apelidoPesquisa');
+let editComandoPesquisa     = modalEditarPesquisa.querySelector('#comandoPesquisa');
+
+let modalEditarConexao_BotaoSalvar        = modalEditarConexao.querySelector('.done');
+let modalEditarConexao_BotaoClose         = modalEditarConexao.querySelector('.close');
+
+let modalEditarPesquisa_BotaoSalvar       = modalEditarPesquisa.querySelector('.done');
+let modalEditarPesquisa_BotaoClose        = modalEditarPesquisa.querySelector('.close');
+
+let modalConfirmarExclusao_BotaoConfirmar = modalConfirmarExclusao.querySelector('.done');
+let modalConfirmarExclusao_BotaoClose     = modalConfirmarExclusao.querySelector('.close');
 
 let modalAbout_Botaoclose                 = modalAbout.querySelector('.close');
-let modalConfirmarExclusao_BotaoClose     = modalConfirmarExclusao.querySelector('.close');
-let modalConfirmarExclusao_BotaoConfirmar = modalConfirmarExclusao.querySelector('.done');
 
-botaoAdd.style.display = 'none';
 
 let Conexoes = {"Dados" :[
     {'id': 1,
@@ -46,9 +65,18 @@ let Pesquisas = {"Dados": [
 
 ]};
 
+
 modalAbout_Botaoclose.addEventListener('click', (event) =>  {
     modalAbout.close();
     FecharMenu();
+});
+
+modalEditarConexao_BotaoClose.addEventListener('click', (event) =>  {
+    modalEditarConexao.close();
+});
+
+modalEditarPesquisa_BotaoClose.addEventListener('click', (event) =>  {
+    modalEditarPesquisa.close();
 });
 
 modalConfirmarExclusao_BotaoClose.addEventListener('click', (event) =>  {
@@ -94,6 +122,23 @@ document.addEventListener('click', (event) => {
         var button = event.target.parentElement;
 
         switch (button.dataset['action']) {
+            case 'adicionar':
+                if (listaPrincipal.dataset['info'] == 'CONEXOES') {
+                    PreencherFormEdicaoConexao(-1);
+                    modalEditarConexao.showModal();
+                } else if (listaPrincipal.dataset['info'] == 'PESQUISAS') {
+                    PreencherFormEdicaoPesquisa(-1);
+                    modalEditarPesquisa.showModal();
+                }
+                break;
+            case 'editarConexao':
+                PreencherFormEdicaoConexao(button.dataset['item']);
+                modalEditarConexao.showModal();
+                break
+            case 'editarPesquisa':
+                PreencherFormEdicaoPesquisa(button.dataset['item']);
+                modalEditarPesquisa.showModal();
+                break
             case 'excluirConexao':
                 modalConfirmarExclusao.Lista     = Conexoes;
                 modalConfirmarExclusao.IdExcluir = button.dataset['item'];
@@ -114,6 +159,7 @@ document.addEventListener('click', (event) => {
 
 function ListarConexoes () {
     var htmlConexoes = '';
+    Conexoes.Dados.sort((a, b) => a.apelido.toUpperCase() > b.apelido.toUpperCase() ? 1 : -1);
     Conexoes.Dados.forEach(Conexao => {
         htmlConexoes +=
             `<li class="lista-principal-conexao">
@@ -142,6 +188,7 @@ function ListarConexoes () {
             </li>`;
     })
 
+    listaPrincipal.dataset['info'] = 'CONEXOES';
     listaPrincipal.innerHTML = htmlConexoes;
     botaoAdd.style.display = 'inherit';
 };
@@ -149,6 +196,7 @@ function ListarConexoes () {
 
 function ListarPesquisas () {
     var htmlPesquisas = '';
+    Pesquisas.Dados.sort((a, b) => a.apelido.toUpperCase() > b.apelido.toUpperCase() ? 1 : -1);
     Pesquisas.Dados.forEach(Pesquisa => {
         htmlPesquisas +=
             `<li class="lista-principal-pesquisa">
@@ -177,6 +225,92 @@ function ListarPesquisas () {
             </li>`;
     })
 
+    listaPrincipal.dataset['info'] = 'PESQUISAS';
     listaPrincipal.innerHTML = htmlPesquisas;
     botaoAdd.style.display = 'inherit';
+}
+
+
+function PreencherFormEdicaoConexao(id) {
+    editIdConexao.value = id;
+    if (id < 0) {
+        editApelidoConexao.value = '';
+        editCaminhoConexao.value = '';
+    } else {
+        let index = Conexoes.Dados.findIndex(conexao => conexao.id == id);
+        editApelidoConexao.value = Conexoes.Dados[index].apelido;
+        editCaminhoConexao.value = Conexoes.Dados[index].caminho;
+    }
+}
+
+function PreencherFormEdicaoPesquisa(id) {
+    editIdPesquisa.value = id;
+    if (id < 0) {
+        editApelidoPesquisa.value = '';
+        editComandoPesquisa.value = '';
+    } else {
+        let index = Pesquisas.Dados.findIndex(pesquisa => pesquisa.id == id);
+        editApelidoPesquisa.value = Pesquisas.Dados[index].apelido;
+        editComandoPesquisa.value = Pesquisas.Dados[index].comando;
+    }
+}
+
+modalEditarConexao_BotaoSalvar.addEventListener('click', (event) =>  {
+    if (editApelidoConexao.value.trim() == '' || editCaminhoConexao.value.trim() == '') return;
+
+    id = editIdConexao.value;
+
+    let index;
+
+    if (id < 0) {
+        let max = MaxArray(Conexoes.Dados, "id") || 0;
+        Conexoes.Dados.push({"id": max + 1});
+        index = Conexoes.Dados.length - 1;
+    } else {
+        index = Conexoes.Dados.findIndex(conexao => conexao.id == id);
+    }
+
+    Conexoes.Dados[index].apelido = editApelidoConexao.value.trim();
+    Conexoes.Dados[index].caminho = editCaminhoConexao.value.trim();
+
+    ListarConexoes();
+    modalEditarConexao.close();
+});
+
+
+modalEditarPesquisa_BotaoSalvar.addEventListener('click', (event) =>  {
+    if (editApelidoPesquisa.value.trim() == '' || editComandoPesquisa.value.trim() == '') return;
+
+    id = editIdPesquisa.value;
+
+    let index;
+
+    if (id < 0) {
+        let max = MaxArray(Pesquisas.Dados, "id") || 0;
+        Pesquisas.Dados.push({"id": max + 1});
+        index = Pesquisas.Dados.length - 1;
+    } else {
+        index = Pesquisas.Dados.findIndex(pesquisa => pesquisa.id == id);
+    }
+
+    Pesquisas.Dados[index].apelido = editApelidoPesquisa.value.trim();
+    Pesquisas.Dados[index].comando = editComandoPesquisa.value.trim();
+
+    ListarPesquisas();
+    modalEditarPesquisa.close();
+});
+
+
+
+/**
+ * @return {number}
+ */
+function MaxArray(Array, atributo) {
+  let max = null;
+  Array.forEach(elemento => {
+      if (elemento[atributo] > max) {
+          max = elemento[atributo]
+      }
+  });
+  return max;
 }
