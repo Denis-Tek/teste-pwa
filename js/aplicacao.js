f3b853704a70d92650fd3cebb6ad37982c369921
desenvolvedor <denisuba@gmailcom>
2020-05-14 17:03:59 -0300

    favicon

--- js/aplicacao.js
+++ js/aplicacao.js
@@ -403,10 +403,3 @@ function MaxArray(Array, atributo) {
 
 
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./service_worker.js')
-            .then( (regis) => {console.log('Service worker registrado!', regis)} )
-            .catch( (erro) => {console.log('Erro ao registrar o meu service worker', erro) })
-    })
-}
